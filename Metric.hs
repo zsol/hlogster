616ed05429dbe2ff621075b1a5f938a8fd080f3f
Zsolt Dollenstein <zsol.zsol@gmail.com>
2012-07-13 15:39:02 +0200

    introduce intermediate MetricState object, which can be combined with other instances of itself or output as a result

--- Metric.hs
+++ Metric.hs
@@ -1,6 +1,4 @@
 module Metric (
-  Metric(Metric),
-  MetricFun,
   getResults,
   parseConfig
   ) where
@@ -11,20 +9,46 @@ import Parsers
 import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Data.ByteString.Char8 as SB
 
-type MetricFun = [B.ByteString] -> [Float]
-data Metric = Metric {
-  apply :: MetricFun,
-  names :: [String]
-  }
 
-getResults :: Metric -> [B.ByteString] -> [(String, String)]
-getResults metric input = zip (names metric) $ map show $ apply metric input
+type Metric = [B.ByteString] -> MetricState
+type Results = [(String, String)]
 
-toList :: t -> [t]
-toList a = [a]
+class IMetricState a
+  where
+    combine :: a -> a -> a
+    toResults :: a -> Results
 
-countEvents :: SB.ByteString -> SB.ByteString -> MetricFun
-countEvents category eventId = toList . fromIntegral . length . events
+data MetricState =
+  CounterMetricState String Float |
+  TimingMetricState {
+    name :: String,
+    min' :: Float,
+    max' :: Float,
+    avg' :: Float,
+    num' :: Float
+    }
+
+instance IMetricState MetricState
+  where
+    combine (CounterMetricState name' a) (CounterMetricState _ b) = CounterMetricState name' (a+b) -- todo: check
+    combine (TimingMetricState {name = aname, min' = amin, max' = amax, avg' = aavg, num' = anum}) (TimingMetricState {min' = bmin, max' = bmax, avg' = bavg, num' = bnum}) = TimingMetricState {
+      name = aname,
+      min' = min amin bmin,
+      max' = max amax bmax,
+      avg' = ((aavg * anum) + (bavg * bnum)) / (anum + bnum),
+      num' = anum + bnum
+      }
+    combine _ _ = undefined
+
+    toResults (CounterMetricState name' a) = [(name', show a)]
+    toResults a = [ (name a ++ "." ++ x, show $ y a) | (x, y) <- zip ["min", "max", "avg", "count"] [min', max', avg', num']]
+
+
+getResults :: Metric -> [B.ByteString] -> Results
+getResults metric input = toResults $ metric input
+
+countEvents :: SB.ByteString -> SB.ByteString -> String -> [B.ByteString] -> MetricState
+countEvents category eventId nameString input = CounterMetricState nameString (fromIntegral $ length $ events input)
   where
     events [] = []
     events (l:ls)
@@ -38,10 +62,10 @@ makeEventCounter :: String -> JSObject JSValue -> Result Metric
 makeEventCounter nameString obj = let (!) = flip valFromObj in do
   category <- obj ! "category"
   event <- obj ! "event"
-  return $ Metric {names = [nameString], apply = countEvents (SB.pack category) (SB.pack event)}
+  return $ countEvents (SB.pack category) (SB.pack event) nameString
 
-countFields :: [(Int, SB.ByteString)] -> MetricFun
-countFields spec = toList . fromIntegral . length . matchingFields
+countFields :: [(Int, SB.ByteString)] -> String -> [B.ByteString] -> MetricState
+countFields spec nameString input = CounterMetricState nameString (fromIntegral $ length $ matchingFields input)
   where
     matchingFields [] = []
     matchingFields (l:ls)
@@ -54,9 +78,8 @@ countFields spec = toList . fromIntegral . length . matchingFields
 makeFieldCounter :: String -> JSObject JSValue -> Result Metric
 makeFieldCounter nameString obj = let (!) = flip valFromObj in do
   fieldsSpec <- obj ! "fields"
-  return $ Metric {names = [nameString], apply = countFields fieldsSpec}
+  return $ countFields fieldsSpec nameString
   
-
 makeMetric :: JSObject JSValue -> Result Metric
 makeMetric obj = let (!) = flip valFromObj in do
   typeString <- obj ! "type"
@@ -72,3 +95,4 @@ parseConfig = toEither . (decode >=> mapM makeMetric)
   where
     toEither (Ok a)    = Right a
     toEither (Error a) = Left $ "Failed to parse config: " ++ a
+
--- Parsers.hs
+++ Parsers.hs
@@ -1,11 +1,22 @@
 {-# LANGUAGE NoMonomorphismRestriction, FlexibleContexts #-}
 module Parsers
-       (getCategoryAndEvent, getFields)
+       (getCategoryAndEvent, getFields, getFieldsWithTime)
        where
 import Data.Attoparsec.Lazy
 import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Data.ByteString.Char8 as SB
 import Data.List (sort)
+import Data.Time.Clock
+import Data.Time.Format
+import System.Locale
+
+datetime :: Parser UTCTime
+datetime = do
+  date <- field
+  time <- field
+  case parseTime defaultTimeLocale "%F %T,%q" ((SB.unpack date) ++ " " ++ (SB.unpack time)) of
+    Just a -> return a
+    _      -> fail "Invalid time format"
 
 spaces :: Parser ()
 spaces = skipWhile $ inClass " \t"
@@ -28,8 +39,17 @@ fields (n:ns) = do
   rest <- fields newns
   return (ret:rest)
 
+fieldsWithTime :: [Int] -> Parser (UTCTime, [SB.ByteString])
+fieldsWithTime l = do
+  time <- datetime
+  rest <- fields $ map (flip (-) 2) l
+  return (time, rest)
+
 getFields :: [Int] -> B.ByteString -> Either String [SB.ByteString]
 getFields numbers = eitherResult . parse (fields $ sort numbers)
+
+getFieldsWithTime :: [Int] -> B.ByteString -> Either String (UTCTime, [SB.ByteString])
+getFieldsWithTime numbers = eitherResult . parse (fieldsWithTime $ sort numbers)
 
 categoryAndEvent :: Parser (SB.ByteString, SB.ByteString)
 categoryAndEvent = do
