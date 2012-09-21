119ac2faa7c2233a66f11f909a16ba6b6eca3c5c
Zsolt Dollenstein <zsol.zsol@gmail.com>
2012-09-20 21:44:42 -0700

    added two tests and discovered a bug

--- Test.hs
+++ Test.hs
@@ -3,6 +3,7 @@ import Metric
 import Test.HUnit
 import qualified Data.ByteString.Char8 as SB
 import qualified Data.ByteString.Lazy.Char8 as B
+import Data.Either
 
 data DummyState = DS deriving (Show, Eq)
 
@@ -16,23 +17,8 @@ dummyMetric _ = DS
 dummyInput = map B.pack [ "2012-07-05 09:00:00,759"
                         , "2012-07-05 09:00:01,764"
                         , "2012-07-05 09:00:02,765"
+                        , "2012-07-05 09:00:00,801"
                         , "2012-07-05 09:00:03,765"
-                        , "2012-07-05 09:00:04,771"
-                        , "2012-07-05 09:00:05,773"
-                        , "2012-07-05 09:00:01,776"
-                        , "2012-07-05 09:00:01,776"
-                        , "2012-07-05 09:00:02,783"
-                        , "2012-07-05 09:00:06,784"
-                        , "2012-07-05 09:00:00,784"
-                        , "2012-07-05 09:00:06,795"
-                        , "2012-07-05 09:00:06,796"
-                        , "2012-07-05 09:00:06,800"
-                        , "2012-07-05 09:00:00,801"
-                        , "2012-07-05 09:00:00,801"
-                        , "2012-07-05 09:00:00,816"
-                        , "2012-07-05 09:00:00,816"
-                        , "2012-07-05 09:00:00,817"
-                        , "2012-07-05 09:00:00,818"
                         ]
              
 someTime :: SB.ByteString
@@ -57,7 +43,7 @@ justLaterBuf = insertIntoBuf DS empty someLaterTime
 tests :: Test
 tests = test [ "Insert into empty buffer" ~: size (insertIntoBuf DS empty someTime) @?= 1
               , "Empty buffer is empty" ~: size empty @?= 0
-              , "Insert same timestamp twice combines" ~: size (insertIntoBuf DS someBuf someTime) @?= 1
+              , "Insert same timestamp twice combines" ~: size (insertIntoBuf DS someLaterBuf someTime) @?= 2
               , "Insert newer timestamps extends" ~: size someLaterBuf @?= 2
               , "Insert order does not matter" ~: reverseSomeLaterBuf @?= someLaterBuf
               , "downSizeBuf is noop when buf too small" ~: downSizeBuf emptyBuf 1 @?= (emptyBuf, Nothing)
@@ -67,4 +53,6 @@ tests = test [ "Insert into empty buffer" ~: size (insertIntoBuf DS empty someTi
               , "processLine inserts if too small" ~: processLine 2 dummyMetric B.empty someTime empty @?= (someBuf, Nothing)
               , "processLine drops if very old and at size" ~: processLine 1 dummyMetric B.empty someOldTime someBuf @?= (someBuf, Nothing)
               , "processLine downsizes if too big" ~: processLine 1 dummyMetric B.empty someLaterTime someBuf @?= (justLaterBuf, Just (someTime, DS))
+              , "processLine should merge ABA style input" ~: processLine 2 dummyMetric B.empty someTime someLaterBuf @?= (someLaterBuf, Nothing)
+              , "Out of order insertion should combine" ~: size (foldl (insertIntoBuf DS) empty (rights $ map getTime dummyInput)) @?= 4
               ]
