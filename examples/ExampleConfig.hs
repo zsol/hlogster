f4536c59c5bded9363bf55dc336193212c330e6b
Zsolt Dollenstein <zsol.zsol@gmail.com>
2012-09-29 17:18:09 +0200

    add presentationBackup config

--- examples/ExampleConfig.hs
+++ examples/ExampleConfig.hs
@@ -1,16 +1,20 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell,NamedFieldPuns #-}
 module Config (config) where
 
 import ConfigBase
 
+addPrefix :: Config -> Config
+addPrefix r = let origName = name r in r {name = "hlogster." ++ origName}
 
 config :: [Config]
-config =
-  [ EventCounter {name = "presentations_delete", category = "usage", event = "presentations_delete"}
-  , EventCounter {name = "feature.label_zoom", category = "feature", event = "label_zoom"}
-  , FieldCounter {name = "something.different",
-                  fields = [Field {index = 4, match = "storage"},
-                            Field {index = 7, match = "and_now_for_something_completely_different"}]}
-  , RegexCounter {name = "timing", regex = $(compileRegex "^[^ ]+ [^ ]+ [^ ]+ storage INFO [^ ]+ timing ([^ ]+) ([0-9.]+) ([^ ]+) .*$")}
-  , RegexCounter {name = "item_read", regex = $(compileRegex "^[^ ]+ [^ ]+ [^ ]+ storage INFO [0-9]+:item_read")}
+config = map addPrefix $ concat [
+  presentationBackup
+  ]
+
+presentationBackup :: [Config]
+presentationBackup =
+  [ FieldCounter {name = "presentation_backup.successful_backups", fields =
+                     [Field {index = 7, match = "success"},
+                      Field {index = 8, match = "presentation"}]}
+  , RegexCounter {name = "presentation_backup.exceptions", regex = $(compileRegex "^\\[[^ ]+\\] [^ ]+ [^ ]+ INFO [^ ]+ .*[Ee]xception.*")}
   ]
