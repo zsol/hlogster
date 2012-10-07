31cc75d3ac7a2870ea2d387cda4df93b3a214d6b
Zsolt Dollenstein <zsol.zsol@gmail.com>
2012-10-07 21:21:22 +0200

    drop network-fancy as it was broken

--- Carbon.hs
+++ Carbon.hs
@@ -1,27 +1,19 @@
 module Carbon
-       (sendTo, oam2, oam3, localhost, withStream, sendToCarbon)
+       (sendToCarbon)
        where
 
 import System.IO (hPutStrLn, Handle)
-import Network.Fancy
 import Data.Time.Clock.POSIX
 import Control.Monad (liftM)
-oam2 :: Address
-oam2 = IP "oam2.us.prezi.private" 2003
-oam3 :: Address
-oam3 = IP "oam3.us.prezi.private" 2003
-localhost :: Address
-localhost = IP "localhost" 2003
 
 sendToCarbon :: (String, String, String) -> Handle -> IO ()
 sendToCarbon (key, value, "") handle = now >>= \time -> hPutStrLn handle (key ++ " " ++ value ++ " " ++ time)
 sendToCarbon (key, value, time) handle = hPutStrLn handle (key ++ " " ++ value ++ " " ++ time)
 
-sendTo :: Address -> (String, String, String) -> IO ()
-sendTo address' = withStream address' . sendToCarbon
-
 now :: IO String
 now = liftM (init . show) getPOSIXTime
+
+
 
 {-
 try it with:
