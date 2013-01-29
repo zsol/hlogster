328f8529708353c031bfef7a520d1a3bd4eb0f2a
Zsolt Dollenstein <zsol.zsol@gmail.com>
2013-01-30 00:23:00 +0100

    RegexCounter optimiziation

--- Metrics/RegexCounter.hs
+++ Metrics/RegexCounter.hs
@@ -4,6 +4,7 @@ import Metrics.Common
 import Text.Regex.Base.RegexLike (matchCount)
 import           Text.Regex.PCRE.ByteString.Lazy
 import qualified Data.ByteString.Lazy.Char8      as B
+import Control.Parallel.Strategies
 
 countRegexen :: Regex -> String -> [B.ByteString] -> MetricState
-countRegexen regex nameString input = CounterMetricState nameString (fromIntegral $ sum $ map (matchCount regex) input)
+countRegexen regex nameString input = CounterMetricState nameString (fromIntegral $ sum $ parMap rdeepseq (matchCount regex) input)
